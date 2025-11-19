617b50179423fbd2b2558f753f5ebcc29ff69c56
Engineer Tony <wangolotony4@gmail.com>
2025-11-20 02:31:29 +0300

    Fix: Use standard DRF pagination class instead of custom one

--- config/settings/render.py
+++ config/settings/render.py
@@ -106,7 +106,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'utils.pagination.StandardPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': [
         'rest_framework.filters.SearchFilter',
